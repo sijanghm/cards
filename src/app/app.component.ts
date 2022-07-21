c00c0c0ce113483f8faf47ecebea2f4e910b14e7
sijanghm <sijan.ghimire29@gmail.com>
2022-07-21 12:58:50 +0545

    cards project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+  
+  posts=[
+    {
+      title : 'Neat Trees',
+      imageUrl: 'assets/img/tree.jpeg',
+      username : 'sijan',
+      content: 'Saw a tree during my hike'
+    },
+    {
+      title : 'Snowy Mountain',
+      imageUrl: 'assets/img/mountain.jpeg',
+      username : 'hi',
+      content: 'Saw a snowy mountain during my hike'
+    },
+    {
+      title : 'Mountain Bike',
+      imageUrl: 'assets/img/biking.jpeg',
+      username : 'cycle_lover',
+      content: 'Had Awesome exprience with biking'
+    }
+  ]
 }
